e8a4f0cdc94238bf5da59ca4acfdc48e103e2248
tulolo287 <iv1055@yandex.ru>
2025-09-27 23:07:42 +0300

    add enemy following

--- js/entities/Enemy.js
+++ js/entities/Enemy.js
@@ -3,53 +3,60 @@ class Enemy extends Entity {
     super(game, x, y, width, height, imageSrc)
     this.dir = 'left'
     this.image.src = imageSrc ?? "./images/tile-E.png"
+
+    this.state = ['walk', 'attack']
+    this.walkSpeed = 5
+    this.attackSpeed = 15
+    this.attackDistance = 150
+    this.disX = 0
+    this.disY = 0
+    this.distance = 0
     this.health = this.width
     this.delete = false
     this.distanceTravel = Math.floor(Math.random() * 50 + 10)
     this.step = 0
 
     this.game.events.on("PLAYER_ATTACK", data => {
-      if (this.isCollidedWith(this, this.game.cellSize, data.item) ||
-        this.x === data.item.x && this.y === data.item.y) {
+      if (this.distance <= this.game.cellSize) {
         this.health -= data.item.attackStrength
+        if (Math.abs(this.disX) > Math.abs(this.disY)) {
+          this.x = data.item.faceLeft === false ? this.x += this.game.cellSize : this.x -= this.game.cellSize
+        } else if (Math.abs(this.disX) < Math.abs(this.disY)) {
+          this.y = this.disY > 0 ? this.y += this.game.cellSize : this.y -= this.game.cellSize
+        } 
         data.item.isAttacked = false
       }
     })
   }
 
-  isCollidedWith(obj1, distance, obj2) {
-    if (obj1.x + distance === obj2.x && obj1.y === obj2.y) {
-      return 'right'
-    }
-    if (obj1.x - distance === obj2.x && obj1.y === obj2.y) {
-      return 'left'
-    }
-    if (obj1.y + distance === obj2.y && obj1.x === obj2.x) {
-      return 'down'
-    }
-    if (obj1.y - distance === obj2.y && obj1.x === obj2.x) {
-      return 'up'
-    }
-    return false
-  }
 
   update(delta) {
     super.update(delta)
-    this.step += 1
-    if (this.step % this.distanceTravel === 0) {
-      this.step = 0
-      const newDir = Math.floor(Math.random() * 5)
-      this.dir = ['right', 'left', 'up', 'down'][newDir]
-    }
-    
-    const playerDir = this.isCollidedWith(this, this.game.cellSize, this.game.player)
-    if (playerDir) {
-      this.x = this.game.player.x
-      this.y = this.game.player.y
-      this.dir = playerDir
+
+    const { disX, disY, distance } = getDistance(this, this.game.player)
+    this.disX = disX
+    this.disY = disY
+    this.distance = distance
+
+    if (this.distance < this.attackDistance) {
+      this.speed = this.attackSpeed
+      if (Math.abs(this.disX) > Math.abs(this.disY)) {
+        this.dir = this.disX > 0 ? 'left' : 'right'
+      } else {
+        this.dir = this.disY > 0 ? 'up' : 'down'
+      }
+    } else {
+      this.speed = this.walkSpeed
+      this.step += 1
+      if (this.step % this.distanceTravel === 0) {
+        this.step = 0
+        const newDir = Math.floor(Math.random() * 5)
+        this.dir = ['right', 'left', 'up', 'down'][newDir]
+      }
     }
 
-    if (this.x === this.game.player.x && this.y === this.game.player.y) {
+
+    if (this.distance < this.game.cellSize) {
       this.game.events.emit("ENEMY_ATTACK", {
         item: this
       })
--- js/utils/index.js
+++ js/utils/index.js
@@ -9,8 +9,36 @@ function getRandom(from, to) {
 function loadImage(item) {
     return new Promise((resolve, reject) => {
         const img = new Image();
-        img.onload = () => resolve({name: item.name, img});
+        img.onload = () => resolve({ name: item.name, img });
         img.onerror = () => reject(new Error(`Failed to load image: ${item}`));
         img.src = item.src;
     });
+}
+
+function isCollidedWith(obj1, distance, obj2) {
+    if (obj1.x + distance === obj2.x && obj1.y === obj2.y) {
+        return 'right'
+    }
+    if (obj1.x - distance === obj2.x && obj1.y === obj2.y) {
+        return 'left'
+    }
+    if (obj1.y + distance === obj2.y && obj1.x === obj2.x) {
+        return 'down'
+    }
+    if (obj1.y - distance === obj2.y && obj1.x === obj2.x) {
+        return 'up'
+    }
+    return false
+}
+
+function isPositionEqual(obj1, obj2) {
+    return obj1.x === obj2.x && obj1.y === obj2.y
+}
+
+function getDistance(obj1, obj2) {
+    const disX = obj1.x - obj2.x
+    const disY = obj1.y - obj2.y
+    const distance = Math.sqrt(disX ** 2 + disY ** 2)
+
+    return { disX, disY, distance }
 }
